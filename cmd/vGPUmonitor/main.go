4b7d244733aa01683c7b654fc802afe1c799195d
limengxuan <391013634@qq.com>
2021-12-20 13:48:55 +0800

    add host monitor

--- cmd/vGPUmonitor/main.go
+++ cmd/vGPUmonitor/main.go
@@ -11,7 +11,17 @@ const shared_directory = "/usr/local/vgpu/shared"
 
 func main() {
 	errchannel := make(chan error)
-	//go serveinfo(errchannel)
+	go serveinfo(errchannel)
+	/*
+		ret := nvml.Init()
+		if ret != nil {
+			log.Fatalf("Unable to initialize NVML: %v", ret.Error())
+		}
+		devnum, err := nvml.GetDeviceCount()
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+		fmt.Println("devnum=", devnum)*/
 	go initmetrics()
 	for {
 		err := <-errchannel
