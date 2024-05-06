ae9ab0462c0981559583c127b055bc2f8b84221f
Morlay <morlay.null@gmail.com>
2024-05-06 12:07:03 +0800

    setup multi-arch build
    Signed-off-by: Morlay <morlay.null@gmail.com>

--- hack/build.sh
+++ hack/build.sh
@@ -22,29 +22,25 @@ export SHORT_VERSION
 export COMMIT_CODE
 export VERSION="${SHORT_VERSION}-${COMMIT_CODE}"
 export LATEST_VERSION="latest"
-export GOLANG_IMAGE="golang:1.21-bullseye"
-export NVIDIA_IMAGE="nvidia/cuda:12.2.0-devel-ubuntu20.04"
+export GOLANG_IMAGE=${GOLANG_IMAGE:-"golang:1.21-bullseye"}
+export NVIDIA_IMAGE=${NVIDIA_IMAGE:-"nvidia/cuda:12.2.0-devel-ubuntu20.04"}
+export NVIDIA_RUNTIME_IMAGE=${NVIDIA_RUNTIME_IMAGE:-"nvidia/cuda:12.2.0-base-ubuntu22.04"}
 export DEST_DIR="/usr/local"
 
 IMAGE=${IMAGE-"projecthami/hami"}
 
-function go_build() {
-  [[ -z "$J" ]] && J=$(nproc | awk '{print int(($0 + 1)/ 2)}')
-  make -j$J
+function docker_buildx_push() {
+    docker buildx build --push \
+      --platform linux/amd64 \
+      --platform linux/arm64 \
+      --tag "${IMAGE}:${SHORT_VERSION}" \
+      --tag "${IMAGE}:${LATEST_VERSION}" \
+      --build-arg VERSION="${VERSION}" \
+      --build-arg GOLANG_IMAGE=${GOLANG_IMAGE} \
+      --build-arg NVIDIA_IMAGE=${NVIDIA_IMAGE} \
+      --build-arg NVIDIA_RUNTIME_IMAGE=${NVIDIA_RUNTIME_IMAGE} \
+      --build-arg DEST_DIR=${DEST_DIR} \
+      -f docker/Dockerfile .
 }
 
-function docker_build() {
-    docker build --build-arg VERSION="${VERSION}" --build-arg GOLANG_IMAGE=${GOLANG_IMAGE} --build-arg NVIDIA_IMAGE=${NVIDIA_IMAGE} --build-arg DEST_DIR=${DEST_DIR} -t "${IMAGE}:${VERSION}" -f docker/Dockerfile .
-    docker tag "${IMAGE}:${VERSION}" "${IMAGE}:${SHORT_VERSION}"
-    docker tag "${IMAGE}:${VERSION}" "${IMAGE}:${LATEST_VERSION}"
-}
-
-function docker_push() {
-    #docker push "${IMAGE}:${VERSION}"
-    docker push "${IMAGE}:${SHORT_VERSION}"
-    docker push "${IMAGE}:${LATEST_VERSION}"
-}
-
-go_build
-docker_build
-docker_push
+docker_buildx_push
