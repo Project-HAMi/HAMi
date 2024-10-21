b0873aaa702e3aac82a5813e84b9095f42e7131e
chaunceyjiang <chaunceyjiang@gmail.com>
2024-10-21 15:20:51 +0800

    Fix pod assignment issue when pod already has a node assigned
    Signed-off-by: chaunceyjiang <chaunceyjiang@gmail.com>

--- pkg/scheduler/webhook_test.go
+++ pkg/scheduler/webhook_test.go
@@ -27,6 +27,10 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/serializer"
 	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
+
+	"github.com/Project-HAMi/HAMi/pkg/device"
+	"github.com/Project-HAMi/HAMi/pkg/device/nvidia"
+	"github.com/Project-HAMi/HAMi/pkg/scheduler/config"
 )
 
 func TestHandle(t *testing.T) {
@@ -84,6 +88,72 @@ func TestHandle(t *testing.T) {
 	resp := wh.Handle(context.Background(), req)
 	if !resp.Allowed {
 		t.Errorf("Expected allowed response, but got: %v", resp)
+	}
+
+}
+
+func TestPodHasNodeName(t *testing.T) {
+	config.SchedulerName = "hami-scheduler"
+	nvidia.ResourceName = "hami.io/gpu"
+	nvidia.ResourceMem = "hami.io/gpumem"
+	nvidia.ResourceMemPercentage = "hami.io/gpumem-percentage"
+	nvidia.ResourceCores = "hami.io/gpucores"
+	device.InitDevices()
+	// create a Pod object
+	pod := &corev1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "test-pod",
+			Namespace: "default",
+		},
+		Spec: corev1.PodSpec{
+			Containers: []corev1.Container{
+				{
+					Name: "container1",
+					SecurityContext: &corev1.SecurityContext{
+						Privileged: nil,
+					},
+					Resources: corev1.ResourceRequirements{
+						Limits: corev1.ResourceList{
+							"hami.io/gpu": resource.MustParse("1"),
+						},
+					},
+				},
+			},
+			NodeName: "test-node",
+		},
+	}
+
+	// encode the Pod object
+	scheme := runtime.NewScheme()
+	corev1.AddToScheme(scheme)
+	codec := serializer.NewCodecFactory(scheme).LegacyCodec(corev1.SchemeGroupVersion)
+	podBytes, err := runtime.Encode(codec, pod)
+	if err != nil {
+		t.Fatalf("Error encoding pod: %v", err)
+	}
+
+	// create an AdmissionRequest object
+	req := admission.Request{
+		AdmissionRequest: admissionv1.AdmissionRequest{
+			UID:       "test-uid",
+			Namespace: "default",
+			Name:      "test-pod",
+			Object: runtime.RawExtension{
+				Raw: podBytes,
+			},
+		},
+	}
+
+	// create a WebHook object
+	wh, err := NewWebHook()
+	if err != nil {
+		t.Fatalf("Error creating WebHook: %v", err)
+	}
+
+	// call the Handle method
+	resp := wh.Handle(context.Background(), req)
+	if resp.Allowed {
+		t.Errorf("Expected denied response, but got: %v", resp)
 	}
 
 }
