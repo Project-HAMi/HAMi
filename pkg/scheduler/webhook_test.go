5d795630b92c821d9312480eff86bda58cd56d8e
chaunceyjiang <chaunceyjiang@gmail.com>
2025-01-08 14:53:40 +0800

    feat: The pod is in privileged mode and has declared HAMi-related resources. Its creation is denied.
    Signed-off-by: chaunceyjiang <chaunceyjiang@gmail.com>

--- pkg/scheduler/webhook_test.go
+++ pkg/scheduler/webhook_test.go
@@ -26,6 +26,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/serializer"
+	"k8s.io/utils/ptr"
 	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
 
 	"github.com/Project-HAMi/HAMi/pkg/device"
@@ -123,6 +124,74 @@ func TestPodHasNodeName(t *testing.T) {
 				},
 			},
 			NodeName: "test-node",
+		},
+	}
+
+	// encode the Pod object
+	scheme := runtime.NewScheme()
+	corev1.AddToScheme(scheme)
+	codec := serializer.NewCodecFactory(scheme).LegacyCodec(corev1.SchemeGroupVersion)
+	podBytes, err := runtime.Encode(codec, pod)
+	if err != nil {
+		t.Fatalf("Error encoding pod: %v", err)
+	}
+
+	// create an AdmissionRequest object
+	req := admission.Request{
+		AdmissionRequest: admissionv1.AdmissionRequest{
+			UID:       "test-uid",
+			Namespace: "default",
+			Name:      "test-pod",
+			Object: runtime.RawExtension{
+				Raw: podBytes,
+			},
+		},
+	}
+
+	// create a WebHook object
+	wh, err := NewWebHook()
+	if err != nil {
+		t.Fatalf("Error creating WebHook: %v", err)
+	}
+
+	// call the Handle method
+	resp := wh.Handle(context.Background(), req)
+	if resp.Allowed {
+		t.Errorf("Expected denied response, but got: %v", resp)
+	}
+
+}
+
+func TestPodHasPrivilegedContainer(t *testing.T) {
+	config.SchedulerName = "hami-scheduler"
+	device.InitDevicesWithConfig(&device.Config{
+		NvidiaConfig: nvidia.NvidiaConfig{
+			ResourceCountName:            "hami.io/gpu",
+			ResourceMemoryName:           "hami.io/gpumem",
+			ResourceMemoryPercentageName: "hami.io/gpumem-percentage",
+			ResourceCoreName:             "hami.io/gpucores",
+		},
+	})
+	// create a Pod object
+	pod := &corev1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "test-pod",
+			Namespace: "default",
+		},
+		Spec: corev1.PodSpec{
+			Containers: []corev1.Container{
+				{
+					Name: "container1",
+					SecurityContext: &corev1.SecurityContext{
+						Privileged: ptr.To(true),
+					},
+					Resources: corev1.ResourceRequirements{
+						Limits: corev1.ResourceList{
+							"hami.io/gpu": resource.MustParse("1"),
+						},
+					},
+				},
+			},
 		},
 	}
 
