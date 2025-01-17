2d755fc26dda9c239af9b8188fe33807b4eb2348
TimWang <7367474+haitwang-cloud@users.noreply.github.com>
2025-01-17 23:20:53 +0800

    fix: improve error handling and logging in node registration process & refine unhealthy pod detection logic i (#804)
    Signed-off-by: haitwang-cloud <haitao_wht@outlook.com>

--- test/e2e/pod/test_pod.go
+++ test/e2e/pod/test_pod.go
@@ -17,101 +17,163 @@ limitations under the License.
 package e2e
 
 import (
+	"context"
+	"fmt"
 	"strings"
 	"time"
 
 	"github.com/onsi/ginkgo/v2"
 	"github.com/onsi/gomega"
 	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
 
 	"github.com/Project-HAMi/HAMi/test/utils"
 )
 
 var _ = ginkgo.Describe("Pod E2E Tests", ginkgo.Ordered, func() {
-	var clientSet = utils.GetClientSet()
-	var newPod *corev1.Pod
+	const (
+		Namespace      = utils.GPUNameSpace
+		NodeName       = utils.GPUNode
+		NodeLabelKey   = utils.GPUNodeLabelKey
+		NodeLabelValue = utils.GPUNodeLabelValue
+		DeleteTimeout  = 300 * time.Second
+		DeleteInterval = 10 * time.Second
+	)
+
+	var (
+		clientSet = utils.GetClientSet()
+		newPod    *corev1.Pod
+	)
 
 	ginkgo.BeforeAll(func() {
-		ginkgo.By("Add node labeling")
-		_, err := utils.AddNodeLabel(clientSet, utils.GPUNode, utils.GPUNodeLabelKey, utils.GPUNodeLabelValue)
+		ginkgo.By("Adding node labeling")
+		_, err := utils.AddNodeLabel(clientSet, NodeName, NodeLabelKey, NodeLabelValue)
 		gomega.Expect(err).NotTo(gomega.HaveOccurred())
 	})
 
 	ginkgo.AfterEach(func() {
-		ginkgo.By("Deleting pod " + newPod.Name + " in namespace " + newPod.Namespace)
-		err := utils.DeletePod(clientSet, newPod.Namespace, newPod.Name)
-		gomega.Expect(err).NotTo(gomega.HaveOccurred())
-
-		ginkgo.By("Verifying pod " + newPod.Name + " is deleted")
-		gomega.Eventually(func() bool {
-			pods, err := utils.GetPods(clientSet, utils.GPUNameSpace)
-			gomega.Expect(err).NotTo(gomega.HaveOccurred())
-
-			for _, pod := range pods.Items {
-				if strings.Contains(pod.Name, newPod.Name) {
-					return false
-				}
-				return true
-			}
-			return false
-		}, 300*time.Second, 10*time.Second).Should(gomega.BeTrue())
+		ginkgo.By("Cleanup pod after each test")
+		cleanupPod(newPod, clientSet)
 	})
 
 	ginkgo.AfterAll(func() {
-		ginkgo.By("Delete node labeling")
-		_, err := utils.RemoveNodeLabel(clientSet, utils.GPUNode, utils.GPUNodeLabelKey)
+		ginkgo.By("Deleting node labeling")
+		_, err := utils.RemoveNodeLabel(clientSet, NodeName, NodeLabelKey)
 		gomega.Expect(err).NotTo(gomega.HaveOccurred())
 	})
 
-	ginkgo.It("create single pod with CUDA configuration", func() {
+	ginkgo.It("creates a single pod with CUDA configuration", func() {
 		newPod = utils.Pod.DeepCopy()
-		newPod.Name = newPod.Name + utils.GetRandom()
+		newPod.Name += utils.GetRandom()
 
-		ginkgo.By("Creating pod " + newPod.Name + " in namespace " + newPod.Namespace)
-		createdPod, err := utils.CreatePod(clientSet, newPod, newPod.Namespace)
-		gomega.Expect(err).NotTo(gomega.HaveOccurred())
-		gomega.Expect(createdPod.Name).To(gomega.Equal(newPod.Name), "Pod was not created successfully")
+		// Ensure cleanup even if the test fails
+		ginkgo.DeferCleanup(func() {
+			ginkgo.By("DeferCleanup: Deleting pod after test")
+			cleanupPod(newPod, clientSet)
+		})
 
-		ginkgo.By("Verifying pod " + newPod.Name + " in running status")
-		err = utils.WaitForPodRunning(clientSet, newPod.Namespace, newPod.Name)
-		gomega.Expect(err).NotTo(gomega.HaveOccurred())
+		createAndVerifyPod(newPod, clientSet)
 
-		ginkgo.By("Verifying GPU memory in pod " + newPod.Name + " by executing: " + utils.GPUExecuteNvidiaSMI)
+		ginkgo.By("Verifying GPU memory in pod by executing: " + utils.GPUExecuteNvidiaSMI)
 		output, err := utils.KubectlExecInPod(newPod.Namespace, newPod.Name, utils.GPUExecuteNvidiaSMI)
-		gomega.Expect(err).NotTo(gomega.HaveOccurred())
-		gomega.Expect(string(output)).To(gomega.ContainSubstring(utils.GPUPodMemory + utils.GPUPodMemoryUnit))
+		if err != nil {
+			fmt.Printf("nvidia-smi execution error: %v\n", err)
+			fmt.Printf("nvidia-smi output: %s\n", output)
+		}
+		gomega.Expect(err).NotTo(gomega.HaveOccurred(), "nvidia-smi command failed")
 
-		ginkgo.By("Verifying CUDA execution status in pod " + newPod.Name + " by executing: " + utils.GPUExecuteCudaSample)
+		fmt.Println("nvidia-smi output:")
+		fmt.Println(string(output))
+
+		ginkgo.By("Verifying CUDA execution in pod by executing: " + utils.GPUExecuteCudaSample)
 		output, err = utils.KubectlExecInPod(newPod.Namespace, newPod.Name, utils.GPUExecuteCudaSample)
-		gomega.Expect(err).NotTo(gomega.HaveOccurred())
-		gomega.Expect(string(output)).To(gomega.ContainSubstring(utils.GPUCudaTestPass))
+		if err != nil {
+			fmt.Printf("CUDA sample execution error: %v\n", err)
+			fmt.Printf("CUDA sample output: %s\n", output)
+		}
+		gomega.Expect(err).NotTo(gomega.HaveOccurred(), "CUDA sample command failed")
+
+		fmt.Println("CUDA sample output:")
+		fmt.Println(string(output))
 	})
 
 	ginkgo.It("create overcommit pods", func() {
-		newPod = utils.Pod.DeepCopy()
-		newPod.Name = newPod.Name + utils.GetRandom()
-		newPod.Spec.Containers = append(newPod.Spec.Containers, newPod.Spec.Containers[0])
-		newPod.Spec.Containers = append(newPod.Spec.Containers, newPod.Spec.Containers[0])
-		newPod.Spec.Containers[1].Name = newPod.Spec.Containers[0].Name + utils.GetRandom()
-		newPod.Spec.Containers[2].Name = newPod.Spec.Containers[0].Name + utils.GetRandom()
+		newPod = prepareOvercommitPod(utils.Pod.DeepCopy(), Namespace) // Pass the namespace to the helper
 
-		ginkgo.By("Creating pod " + newPod.Name + " within multiple containers in namespace " + newPod.Namespace)
-		createdPod, err := utils.CreatePod(clientSet, newPod, newPod.Namespace)
+		ginkgo.By("Creating overcommit pod in namespace " + Namespace)
+		createdPod, err := utils.CreatePod(clientSet, newPod, Namespace)
 		gomega.Expect(err).NotTo(gomega.HaveOccurred())
 		gomega.Expect(createdPod.Name).To(gomega.Equal(newPod.Name), "Pod was not created successfully")
 
-		ginkgo.By("Verifying pod " + newPod.Name + " is pending due to " + utils.ErrReasonFilteringFailed + utils.ErrMessageFilteringFailed)
+		ginkgo.By("Verifying pod is pending due to filtering")
 		gomega.Eventually(func() bool {
-			events, err := utils.GetPodEvents(clientSet, newPod.Namespace, newPod.Name)
-			gomega.Expect(err).NotTo(gomega.HaveOccurred())
-
-			for _, event := range events {
-				if strings.Contains(event.Reason, utils.ErrReasonFilteringFailed) && strings.Contains(event.Message, utils.ErrMessageFilteringFailed) {
-					return true
-				}
-			}
-			return false
-		}, 300*time.Second, 10*time.Second).Should(gomega.BeTrue())
-
+			return checkPodPendingDueToFiltering(clientSet, newPod)
+		}, DeleteTimeout, DeleteInterval).Should(gomega.BeTrue())
 	})
 })
+
+func cleanupPod(pod *corev1.Pod, clientSet *kubernetes.Clientset) {
+	if podExists(pod.Namespace, pod.Name, clientSet) {
+		ginkgo.By("Deleting pod " + pod.Name + " in namespace " + pod.Namespace)
+		err := utils.DeletePod(clientSet, pod.Namespace, pod.Name)
+		gomega.Expect(err).NotTo(gomega.HaveOccurred())
+
+		ginkgo.By("Verifying pod " + pod.Name + " is deleted")
+		gomega.Eventually(func() bool {
+			return !podExists(pod.Namespace, pod.Name, clientSet)
+		}, 300*time.Second, 10*time.Second).Should(gomega.BeTrue())
+	}
+}
+
+func podExists(namespace, podName string, clientSet *kubernetes.Clientset) bool {
+	pod, err := clientSet.CoreV1().Pods(namespace).Get(context.TODO(), podName, metav1.GetOptions{})
+	return err == nil && pod != nil
+}
+
+func createAndVerifyPod(pod *corev1.Pod, clientSet *kubernetes.Clientset) {
+	ginkgo.By("Creating pod " + pod.Name + " in namespace " + pod.Namespace)
+	createdPod, err := utils.CreatePod(clientSet, pod, pod.Namespace)
+	gomega.Expect(err).NotTo(gomega.HaveOccurred())
+	gomega.Expect(createdPod.Name).To(gomega.Equal(pod.Name), "Pod was not created successfully")
+
+	ginkgo.By("Verifying pod " + pod.Name + " is in running status")
+	err = utils.WaitForPodRunning(clientSet, pod.Namespace, pod.Name)
+	if err != nil {
+		p, _ := clientSet.CoreV1().Pods(pod.Namespace).Get(context.TODO(), pod.Name, metav1.GetOptions{})
+		fmt.Printf("Pod %s/%s status: %v\n", pod.Namespace, pod.Name, p.Status)
+		events, _ := clientSet.CoreV1().Events(pod.Namespace).List(context.TODO(), metav1.ListOptions{
+			FieldSelector: fmt.Sprintf("involvedObject.name=%s", pod.Name),
+		})
+		for _, event := range events.Items {
+			fmt.Printf("Event: %s - %s\n", event.Reason, event.Message)
+		}
+	}
+	gomega.Expect(err).NotTo(gomega.HaveOccurred())
+}
+
+func prepareOvercommitPod(pod *corev1.Pod, namespace string) *corev1.Pod {
+	pod.Name += utils.GetRandom()
+	pod.Namespace = namespace // Ensure the pod's namespace is correctly set
+
+	// Modify pod spec for overcommit scenario
+	pod.Spec.Containers = append(pod.Spec.Containers, pod.Spec.Containers[0])
+	pod.Spec.Containers = append(pod.Spec.Containers, pod.Spec.Containers[0])
+	pod.Spec.Containers[1].Name = pod.Spec.Containers[0].Name + utils.GetRandom()
+	pod.Spec.Containers[2].Name = pod.Spec.Containers[0].Name + utils.GetRandom()
+
+	return pod
+}
+
+func checkPodPendingDueToFiltering(clientSet *kubernetes.Clientset, pod *corev1.Pod) bool {
+	events, err := utils.GetPodEvents(clientSet, pod.Namespace, pod.Name)
+	gomega.Expect(err).NotTo(gomega.HaveOccurred())
+
+	for _, event := range events {
+		if strings.Contains(event.Reason, utils.ErrReasonFilteringFailed) &&
+			strings.Contains(event.Message, utils.ErrMessageFilteringFailed) {
+			return true
+		}
+	}
+	return false
+}
