9c9548494bf13ddd89a19f3b20ecad46a3bf004e
TimWang <7367474+haitwang-cloud@users.noreply.github.com>
2025-01-17 23:20:53 +0800

    fix: improve error handling and logging in node registration process & refine unhealthy pod detection logic i (#804)
    Signed-off-by: haitwang-cloud <haitao_wht@outlook.com>

--- test/e2e/node/test_node.go
+++ test/e2e/node/test_node.go
@@ -18,6 +18,7 @@ package e2e
 
 import (
 	"context"
+	"fmt"
 	"strings"
 	"time"
 
@@ -33,50 +34,72 @@ var _ = ginkgo.Describe("[Node] Node E2E Tests", ginkgo.Ordered, func() {
 	var nodeName string
 
 	ginkgo.BeforeAll(func() {
+		// Get all nodes in the cluster
 		nodes, err := utils.GetNodes(clientSet)
 		gomega.Expect(err).NotTo(gomega.HaveOccurred())
 		gomega.Expect(len(nodes.Items)).To(gomega.BeNumerically(">", 0), "No nodes available for testing")
 
-		nodeName = nodes.Items[0].Name
+		// Select a node with GPU resources
+		for _, node := range nodes.Items {
+			if _, exists := node.Status.Capacity["nvidia.com/gpu"]; exists {
+				nodeName = node.Name
+				break
+			}
+		}
+		gomega.Expect(nodeName).NotTo(gomega.BeEmpty(), "No GPU node found")
 	})
 
 	ginkgo.It("verify node with labeling", func() {
+		// Add label to the node
 		ginkgo.By("Updating node " + nodeName + " by labeling " + utils.GPUNodeLabelKey + "=" + utils.GPUNodeLabelValue)
 		_, err := utils.AddNodeLabel(clientSet, nodeName, utils.GPUNodeLabelKey, utils.GPUNodeLabelValue)
+		if err != nil {
+			fmt.Printf("Failed to add label to node %s: %v\n", nodeName, err)
+		}
 		gomega.Expect(err).NotTo(gomega.HaveOccurred())
 
+		// Verify that the label was added correctly
 		ginkgo.By("Checking node " + nodeName + " label")
 		node, err := clientSet.CoreV1().Nodes().Get(context.TODO(), nodeName, metav1.GetOptions{})
 		gomega.Expect(err).NotTo(gomega.HaveOccurred())
 		gomega.Expect(node.Labels[utils.GPUNodeLabelKey]).To(gomega.Equal(utils.GPUNodeLabelValue), "Label was not correctly added")
 
+		// Verify that the HamiDevicePlugin pods are running after labeling
 		ginkgo.By("Checking pods " + utils.HamiDevicePlugin + " running after labeling")
 		gomega.Eventually(func() bool {
 			pods, err := utils.GetPods(clientSet, utils.GPUNameSpace)
 			gomega.Expect(err).NotTo(gomega.HaveOccurred())
 
 			for _, pod := range pods.Items {
-				err := utils.WaitForPodRunning(clientSet, utils.GPUNameSpace, pod.Name)
-				if err != nil {
-					return false
+				if strings.Contains(pod.Name, utils.HamiDevicePlugin) {
+					err := utils.WaitForPodRunning(clientSet, utils.GPUNameSpace, pod.Name)
+					if err != nil {
+						fmt.Printf("Pod %s is not running: %v\n", pod.Name, err)
+						return false
+					}
 				}
-				return true
 			}
-			return false
+			return true
 		}, 300*time.Second, 10*time.Second).Should(gomega.BeTrue())
 	})
 
 	ginkgo.It("verify node after removing label", func() {
+		// Remove label from the node
 		ginkgo.By("Updating node " + nodeName + " by removing label " + utils.GPUNodeLabelKey + "=" + utils.GPUNodeLabelValue)
 		_, err := utils.RemoveNodeLabel(clientSet, nodeName, utils.GPUNodeLabelKey)
+		if err != nil {
+			fmt.Printf("Failed to remove label from node %s: %v\n", nodeName, err)
+		}
 		gomega.Expect(err).NotTo(gomega.HaveOccurred())
 
+		// Verify that the label was removed correctly
 		ginkgo.By("Checking node " + nodeName + " label")
 		node, err := clientSet.CoreV1().Nodes().Get(context.TODO(), nodeName, metav1.GetOptions{})
 		gomega.Expect(err).NotTo(gomega.HaveOccurred())
 		_, exists := node.Labels[utils.GPUNodeLabelKey]
 		gomega.Expect(exists).To(gomega.BeFalse(), "Label was not correctly removed")
 
+		// Verify that the HamiDevicePlugin pods are deleted after removing the label
 		ginkgo.By("Checking pods " + utils.HamiDevicePlugin + " deleted after removing label")
 		gomega.Eventually(func() bool {
 			pods, err := utils.GetPods(clientSet, utils.GPUNameSpace)
@@ -84,6 +107,7 @@ var _ = ginkgo.Describe("[Node] Node E2E Tests", ginkgo.Ordered, func() {
 
 			for _, pod := range pods.Items {
 				if strings.Contains(pod.Name, utils.HamiDevicePlugin) {
+					fmt.Printf("Pod %s still exists\n", pod.Name)
 					return false
 				}
 			}
