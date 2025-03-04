b770801e0af7ae83ca92384d36122354c6d88e51
limengxuan <391013634@qq.com>
2025-03-04 11:18:22 +0800

    fix make test not working properly
    Signed-off-by: limengxuan <391013634@qq.com>

--- pkg/device/cambricon/device_test.go
+++ pkg/device/cambricon/device_test.go
@@ -480,15 +480,6 @@ func Test_setNodeLock(t *testing.T) {
 			},
 			expectErr: false,
 		},
-		/*{
-			name: "no node name",
-			node: corev1.Node{
-				ObjectMeta: metav1.ObjectMeta{
-					Annotations: map[string]string{},
-				},
-			},
-			expectErr: false,
-		},*/
 	}
 
 	client.KubeClient = fake.NewSimpleClientset()
@@ -622,7 +613,6 @@ func Test_LockNode(t *testing.T) {
 		},
 	}
 
-	//client.KubeClient = fake.NewSimpleClientset()
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			node, pod, teardown, clientset := setupTest(t)
