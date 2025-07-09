d9c0bc9c7104455d28a42431c4d3d147039d5a08
P6038829 <guoqiang.zuo1@gientech.com>
2025-07-09 14:21:54 +0800

    Mig supports applying for the same Mem
    Signed-off-by: P6038829 <guoqiang.zuo1@gientech.com>

--- pkg/device/nvidia/device.go
+++ pkg/device/nvidia/device.go
@@ -491,7 +491,7 @@ func (dev *NvidiaGPUDevices) CustomFilterRule(allocated *util.PodDevices, reques
 		for _, val := range toAllocate {
 			found := false
 			for idx := range deviceUsageCurrent.UsageList {
-				if !deviceUsageCurrent.UsageList[idx].InUse && deviceUsageCurrent.UsageList[idx].Memory > val.Usedmem {
+				if !deviceUsageCurrent.UsageList[idx].InUse && deviceUsageCurrent.UsageList[idx].Memory >= val.Usedmem {
 					deviceUsageCurrent.UsageList[idx].InUse = true
 					found = true
 					break
@@ -503,7 +503,7 @@ func (dev *NvidiaGPUDevices) CustomFilterRule(allocated *util.PodDevices, reques
 			}
 		}
 		for idx := range deviceUsageCurrent.UsageList {
-			if !deviceUsageCurrent.UsageList[idx].InUse && deviceUsageCurrent.UsageList[idx].Memory > request.Memreq {
+			if !deviceUsageCurrent.UsageList[idx].InUse && deviceUsageCurrent.UsageList[idx].Memory >= request.Memreq {
 				deviceUsageCurrent.UsageList[idx].InUse = true
 				klog.Infoln("MIG entry device usage true=", deviceUsageCurrent.UsageList, "request", request, "toAllocate", toAllocate)
 				return true
@@ -553,7 +553,7 @@ func (dev *NvidiaGPUDevices) AddResourceUsage(pod *corev1.Pod, n *util.DeviceUsa
 		} else {
 			found := false
 			for idx, val := range n.MigUsage.UsageList {
-				if !val.InUse && val.Memory > ctr.Usedmem {
+				if !val.InUse && val.Memory >= ctr.Usedmem {
 					n.MigUsage.UsageList[idx].InUse = true
 					ctr.Usedmem = n.MigUsage.UsageList[idx].Memory
 					if !strings.Contains(ctr.UUID, "[") {
