dc91e32276a3ff4b8dcc6797c1a8faba2a0d7a76
limengxuan <mengxuan.li@dynamia.ai>
2025-07-30 18:17:48 +0800

    update
    Signed-off-by: limengxuan <mengxuan.li@dynamia.ai>

---
+++ build/config/static_config.h
@@ -0,0 +1,9 @@
+/* #undef MULTIPROCESS_LIMIT_ENABLE */
+/* #undef MEMORY_LIMIT_DEBUG */
+/* #undef DLSYM_HOOK_ENABLE */
+/* #undef DLSYM_HOOK_DEBUG */
+/* #undef HOOK_MEMINFO_ENABLE */
+/* #undef HOOK_NVML_ENABLE */
+extern size_t GIT_HASH_c0f9da6;
+extern size_t GIT_BRANCH_;
+
