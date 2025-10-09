ebe1e84f75ef7e66a53be307b00f3856b8e85ae1
Jifei Wang <jifei.wang@dynamia.ai>
2025-10-09 12:10:30 +0800

    Update e2e-test.sh
    resolve ginkgo version mismatch

--- hack/e2e-test.sh
+++ hack/e2e-test.sh
@@ -13,6 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+#!/usr/bin/env bash
+
 set -o errexit
 set -o nounset
 set -o pipefail
@@ -25,29 +27,17 @@ KUBE_CONF=${2:-""}
 REPO_ROOT=$(dirname "${BASH_SOURCE[0]}")/..
 source "${REPO_ROOT}"/hack/util.sh
 
-if util::cmd_exist ginkgo; then
-  echo "Using ginkgo version:"
-  ginkgo version
-else
-  go install github.com/onsi/ginkgo/v2/ginkgo
-  go get github.com/onsi/gomega/...
-  ginkgo version
-fi
-
-
 if [ -z "${KUBE_CONF}" ]; then
    echo "Error: KUBE_CONF environment variable is not set."
-   return 1
+   exit 1
 fi
 
-# Run e2e
+echo "Using ginkgo version from go.mod:"
+go run github.com/onsi/ginkgo/v2/ginkgo version
+
 if [ "${E2E_TYPE}" == "pullrequest" ] || [ "${E2E_TYPE}" == "release" ]; then
-   ginkgo -v -r --fail-fast  ./test/e2e/ --kubeconfig="${KUBE_CONF}"
-   if [ $? -ne 0 ]; then
-       echo "Error: ginkgo command failed."
-       return 1
-   fi
+   go run github.com/onsi/ginkgo/v2/ginkgo -v -r --fail-fast ./test/e2e/ --kubeconfig="${KUBE_CONF}"
 else
    echo "Invalid E2E Type: ${E2E_TYPE}"
-   return 1
+   exit 1
 fi
