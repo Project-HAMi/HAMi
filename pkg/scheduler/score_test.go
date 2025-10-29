bf2aa652af89715a1af46933c8361f35ddc39da3
luohua13 <jcwang@alauda.io>
2025-10-29 18:58:45 +0800

    fix typo
    Signed-off-by: luohua13 <jcwang@alauda.io>

--- pkg/scheduler/score_test.go
+++ pkg/scheduler/score_test.go
@@ -24,7 +24,7 @@ import (
 	"gotest.tools/v3/assert"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
-	metav1 "k8s.io/apimachinery/pkg/apis/mZZeta/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/klog/v2"
 
 	"github.com/Project-HAMi/HAMi/pkg/device"
