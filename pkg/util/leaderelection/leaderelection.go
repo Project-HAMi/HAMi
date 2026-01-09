ea8dbf3568ad427963e8bb253dd57ebf3b1a73b2
侯雨希 <75366539+peachest@users.noreply.github.com>
2026-01-09 15:56:32 +0800

    Implementing leader-follower high availability using leader-election (#1553)
    * add flags for leaderelection
    
    Signed-off-by: houyuxi <yuxi.hou@transwarp.io>
    
    * add leader manager
    
    Signed-off-by: houyuxi <yuxi.hou@transwarp.io>
    
    * feat: implement `Scheduler` with `LeaderManager`
    
    Signed-off-by: houyuxi <yuxi.hou@transwarp.io>
    
    * feat: implement `readyz` api and readiness probe
    
    Signed-off-by: houyuxi <yuxi.hou@transwarp.io>
    
    * refactor: rename `hostName` param or field to `hostname`
    
    Signed-off-by: houyuxi <yuxi.hou@transwarp.io>
    
    * fix: notify channel
    
    1. update notify channel with buffer of size 1 in leaderManager
    2. init notify chan for dummy
    
    Signed-off-by: houyuxi <yuxi.hou@transwarp.io>
    
    * refactor: rename `isHolder` method to `isHolderOf`
    
    Signed-off-by: houyuxi <yuxi.hou@transwarp.io>
    
    * feat: add  `observedTime` field and `isLeaseValid` method
    
    Signed-off-by: houyuxi <yuxi.hou@transwarp.io>
    
    * test: add test for leaderelection package
    
    Signed-off-by: houyuxi <yuxi.hou@transwarp.io>
    
    * test: add tests for deleting lease
    
    Signed-off-by: houyuxi <yuxi.hou@transwarp.io>
    
    * fix: update readinessProbe using http or https controled by value `.scheduler.admissionWebhook.enabled`
    
    Signed-off-by: houyuxi <yuxi.hou@transwarp.io>
    
    * fix: initializing leader manager with correct param
    
    Signed-off-by: houyuxi <yuxi.hou@transwarp.io>
    
    * add more comments
    
    Signed-off-by: houyuxi <yuxi.hou@transwarp.io>
    
    * add check for empty hostname
    
    Signed-off-by: houyuxi <yuxi.hou@transwarp.io>
    
    * test: add `GinkgoHelper` in assert helper to redirect to point to the correct call when failed
    
    Signed-off-by: houyuxi <yuxi.hou@transwarp.io>
    
    * refactor: update fields order of `Scheduler` type
    
    Signed-off-by: houyuxi <yuxi.hou@transwarp.io>
    
    * add license headers
    
    Signed-off-by: houyuxi <yuxi.hou@transwarp.io>
    
    * fix: do register after schduler started
    
    Signed-off-by: houyuxi <yuxi.hou@transwarp.io>
    
    * feat: add callbacks in leaderelection package and implement `WaitForCacheSynced` for Scheduler with callbacks
    
    Signed-off-by: houyuxi <yuxi.hou@transwarp.io>
    
    * remove useless comment
    
    Signed-off-by: houyuxi <yuxi.hou@transwarp.io>
    
    * feat: check lease valid while checking leader
    
    Signed-off-by: houyuxi <yuxi.hou@transwarp.io>
    
    * feat: add pod anti affinity in deployment when leaderelect is enabled
    
    Signed-off-by: houyuxi <yuxi.hou@transwarp.io>
    
    * fix: list lease in specified namespace
    
    Signed-off-by: houyuxi <yuxi.hou@transwarp.io>
    
    * feat: extract default resync constant
    
    Signed-off-by: houyuxi <yuxi.hou@transwarp.io>
    
    * fix: use RUnlcok after RLock
    
    Signed-off-by: houyuxi <yuxi.hou@transwarp.io>
    
    * trigger action
    
    Signed-off-by: houyuxi <yuxi.hou@transwarp.io>
    
    * ci: extend timeout for Helm deploy in e2e test
    
    Signed-off-by: houyuxi <yuxi.hou@transwarp.io>
    
    * Revert "ci: extend timeout for Helm deploy in e2e test"
    
    This reverts commit e7d9e434cb9bc6ed1f4e9b140bcd3efaeff50d07.
    
    Signed-off-by: houyuxi <yuxi.hou@transwarp.io>
    
    * fix: use `app.kubernetes.io/component` label for podAntiAffinity to avoid pod failed scheduling in postStartHook when deploying on a single node
    
    Signed-off-by: houyuxi <yuxi.hou@transwarp.io>
    
    ---------
    
    Signed-off-by: houyuxi <yuxi.hou@transwarp.io>
    Co-authored-by: houyuxi <yuxi.hou@transwarp.io>

---
+++ pkg/util/leaderelection/leaderelection.go
@@ -0,0 +1,195 @@
+/*
+Copyright 2024 The HAMi Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package leaderelection
+
+import (
+	"strings"
+	"sync"
+	"time"
+
+	coordinationv1 "k8s.io/api/coordination/v1"
+	"k8s.io/client-go/tools/cache"
+)
+
+type LeaderCallbacks struct {
+	// OnStartedLeading is called when starts leading
+	OnStartedLeading func()
+	// OnStoppedLeading is called when stops leading
+	OnStoppedLeading func()
+}
+
+type LeaderManager interface {
+	IsLeader() bool
+
+	cache.ResourceEventHandler
+}
+
+var _ LeaderManager = &leaderManager{}
+
+type leaderManager struct {
+	hostname          string
+	resourceName      string
+	resourceNamespace string
+
+	leaseLock     sync.RWMutex
+	observedLease *coordinationv1.Lease
+	observedTime  time.Time
+
+	callbacks LeaderCallbacks
+
+	cache.FilteringResourceEventHandler
+}
+
+func NewLeaderManager(hostname, namespace, name string, callbacks LeaderCallbacks) *leaderManager {
+	m := &leaderManager{
+		hostname:          hostname,
+		resourceName:      name,
+		resourceNamespace: namespace,
+		callbacks:         callbacks,
+	}
+
+	m.FilteringResourceEventHandler = cache.FilteringResourceEventHandler{
+		FilterFunc: func(obj interface{}) bool {
+			lease := objectToLease(obj)
+			if lease == nil {
+				return false
+			}
+			return lease.Name == m.resourceName && lease.Namespace == m.resourceNamespace
+		},
+		Handler: cache.ResourceEventHandlerFuncs{
+			AddFunc:    m.onAdd,
+			UpdateFunc: m.onUpdate,
+			DeleteFunc: m.onDelete,
+		},
+	}
+
+	return m
+}
+
+func objectToLease(obj interface{}) *coordinationv1.Lease {
+	switch t := obj.(type) {
+	case *coordinationv1.Lease:
+		return t
+	case cache.DeletedFinalStateUnknown:
+		if lease, ok := t.Obj.(*coordinationv1.Lease); ok {
+			return lease
+		}
+	default:
+		return nil
+	}
+	return nil
+}
+
+func (m *leaderManager) setObservedRecord(lease *coordinationv1.Lease) {
+	m.observedLease = lease
+
+	if lease == nil {
+		m.observedTime = time.Time{}
+	} else {
+		m.observedTime = time.Now()
+	}
+}
+
+// onAdd notifies if we are the leader when lease is created.
+func (m *leaderManager) onAdd(obj interface{}) {
+	lease, ok := obj.(*coordinationv1.Lease)
+	if !ok {
+		return
+	}
+
+	m.leaseLock.Lock()
+	defer m.leaseLock.Unlock()
+
+	m.setObservedRecord(lease)
+	// Notify if we are the leader from the very begging
+	if m.isHolderOf(lease) {
+		m.callbacks.OnStartedLeading()
+	}
+}
+
+// onUpdate notifies when we have been elected as leader.
+func (m *leaderManager) onUpdate(oldObj, newObj interface{}) {
+	newLease, ok := newObj.(*coordinationv1.Lease)
+	if !ok {
+		return
+	}
+	oldLease, ok := oldObj.(*coordinationv1.Lease)
+	if !ok {
+		return
+	}
+
+	m.leaseLock.Lock()
+	defer m.leaseLock.Unlock()
+	m.setObservedRecord(newLease)
+
+	// Notify if we have been elected to become the leader
+	if !m.isHolderOf(oldLease) && m.isHolderOf(newLease) {
+		m.callbacks.OnStartedLeading()
+	} else if m.isHolderOf(oldLease) && !m.isHolderOf(newLease) {
+		m.callbacks.OnStoppedLeading()
+	}
+}
+
+func (m *leaderManager) onDelete(obj interface{}) {
+	// Do nothing on delete
+	m.leaseLock.Lock()
+	defer m.leaseLock.Unlock()
+
+	m.setObservedRecord(nil)
+	m.callbacks.OnStoppedLeading()
+}
+
+func (m *leaderManager) isHolderOf(lease *coordinationv1.Lease) bool {
+	// kube-scheduler lease id take format of `hostname + "_" + string(uuid.NewUUID())`
+	return lease.Spec.HolderIdentity != nil && strings.HasPrefix(*lease.Spec.HolderIdentity, m.hostname)
+}
+
+func (m *leaderManager) isLeaseValid(now time.Time) bool {
+	return m.observedTime.Add(time.Second * time.Duration(*m.observedLease.Spec.LeaseDurationSeconds)).After(now)
+}
+
+func (m *leaderManager) IsLeader() bool {
+	m.leaseLock.RLock()
+	defer m.leaseLock.RUnlock()
+
+	if m.observedLease == nil {
+		return false
+	}
+
+	return m.isHolderOf(m.observedLease) && m.isLeaseValid(time.Now())
+}
+
+type dummyLeaderManager struct {
+	elected bool
+	cache.ResourceEventHandlerFuncs
+}
+
+var _ LeaderManager = &dummyLeaderManager{}
+
+// NewDummyLeaderManager creates a dummy leader manager which will not change its elected state during its lifetime.
+// It will always return the elected state passed in the constructor when calling IsLeader() and you will never get notified by it's channel.
+//
+// This is useful when disabling leader-election.
+func NewDummyLeaderManager(elected bool) *dummyLeaderManager {
+	return &dummyLeaderManager{
+		elected: elected,
+	}
+}
+
+func (d *dummyLeaderManager) IsLeader() bool {
+	return d.elected
+}
