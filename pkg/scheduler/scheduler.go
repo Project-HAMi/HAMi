c5a59d11c39422f6fe52f0d01b9729ae376339cd
Shivam Kumar <155747305+maishivamhoo123@users.noreply.github.com>
2026-03-31 11:18:15 +0530

    fix: retain terminating pod in cache to prevent premature eviction (#1719)
    * fix: retain terminating pod in cache to prevent premature eviction
    
    Signed-off-by: maishivamhoo123 <maishivamhoo@gmail.com>
    
    * fix: retain terminating pod in cache to prevent premature eviction
    
    Signed-off-by: maishivamhoo123 <maishivamhoo@gmail.com>
    
    ---------
    
    Signed-off-by: maishivamhoo123 <maishivamhoo@gmail.com>

--- pkg/scheduler/scheduler.go
+++ pkg/scheduler/scheduler.go
@@ -152,6 +152,11 @@ func (s *Scheduler) onAddPod(obj any) {
 		s.podManager.DelPod(pod)
 		return
 	}
+	if util.IsPodTerminating(pod) {
+		klog.V(5).InfoS("Pod is terminating but holding locks, preserving cache", "pod", pod.Name)
+		s.podManager.UpdatePod(pod)
+		return
+	}
 	podDev, _ := device.DecodePodDevices(device.SupportDevices, pod.Annotations)
 	if s.podManager.AddPod(pod, nodeID, podDev) {
 		s.quotaManager.AddUsage(pod, podDev)
