4098c5b2c2d8d944576668b296c066b287348909
代博霖 <15130495867@163.com>
2026-05-27 14:39:26 +0800

    fix: rollback quota usage when filter annotation patch fails (#1898)
    * fix: rollback quota usage when filter annotation patch fails
    
    Rollback quota usage when PatchPodAnnotations fails after AddUsage has been called.
    
    The added flag is used to ensure RmUsage is only called when AddUsage was actually executed.
    
    Signed-off-by: 代博霖 <15130495867@163.com>
    
    * fix: gofmt scheduler
    
    Signed-off-by: 代博霖 <15130495867@163.com>
    
    ---------
    
    Signed-off-by: 代博霖 <15130495867@163.com>

--- pkg/scheduler/scheduler.go
+++ pkg/scheduler/scheduler.go
@@ -782,12 +782,17 @@ func (s *Scheduler) Filter(args extenderv1.ExtenderArgs) (*extenderv1.ExtenderFi
 		val.PatchAnnotations(args.Pod, &annotations, m.Devices)
 	}
 
-	if s.podManager.AddPod(args.Pod, m.NodeID, m.Devices) {
+	added := s.podManager.AddPod(args.Pod, m.NodeID, m.Devices)
+	if added {
 		s.quotaManager.AddUsage(args.Pod, m.Devices)
 	}
+
 	err = util.PatchPodAnnotations(args.Pod, annotations)
 	if err != nil {
 		s.recordScheduleFilterResultEvent(args.Pod, EventReasonFilteringFailed, "", err)
+		if added {
+			s.quotaManager.RmUsage(args.Pod, m.Devices)
+		}
 		s.podManager.DelPod(args.Pod)
 		return nil, err
 	}
