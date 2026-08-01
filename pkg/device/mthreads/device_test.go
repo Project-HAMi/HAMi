9c752f986a7fb9795a9fb90f402740246af43c3d
imantaba <itn.taba@gmail.com>
2026-08-01 09:43:45 +0330

    test(mthreads): cover count=1 requests-only MutateAdmission path
    Add a table case for a container that sets the vgpu count under requests
    only, count=1, with no limits block. This exercises the memok-false branch,
    which also writes to ctr.Resources.Limits and panicked on a nil map before
    the guard added in the previous commit.
    
    Signed-off-by: imantaba <itn.taba@gmail.com>

--- pkg/device/mthreads/device_test.go
+++ pkg/device/mthreads/device_test.go
@@ -103,6 +103,27 @@ func Test_MutateAdmission(t *testing.T) {
 			want: true,
 		},
 		{
+			name: "count one requested without limits",
+			args: struct {
+				ctr *corev1.Container
+				p   *corev1.Pod
+			}{
+				ctr: &corev1.Container{
+					Resources: corev1.ResourceRequirements{
+						Requests: corev1.ResourceList{
+							"mthreads.com/vgpu": *resource.NewQuantity(1, resource.DecimalSI),
+						},
+					},
+				},
+				p: &corev1.Pod{
+					ObjectMeta: metav1.ObjectMeta{
+						Annotations: map[string]string{},
+					},
+				},
+			},
+			want: true,
+		},
+		{
 			name: "don't set to count limit",
 			args: struct {
 				ctr *corev1.Container
