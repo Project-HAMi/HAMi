89319cd2a0ce1e3f94bd9209b63f9d32d7a2afc0
AdityaShome <shomeaditya65@gmail.com>
2026-08-03 13:59:00 +0530

    return an error instead of skipping unresolved MIG UUIDs
    Signed-off-by: AdityaShome <shomeaditya65@gmail.com>

--- pkg/device-plugin/nvidiadevice/nvinternal/plugin/util.go
+++ pkg/device-plugin/nvidiadevice/nvinternal/plugin/util.go
@@ -188,7 +188,7 @@ func GetMigUUIDFromSmiOutput(output string, uuid string, idx int) string {
 	return ""
 }
 
-func GetMigUUIDFromIndex(uuid string, idx int) string {
+func GetMigUUIDFromIndex(uuid string, idx int) (string, error) {
 	defer nvml.Shutdown()
 	if nvret := nvml.Init(); nvret != nvml.SUCCESS {
 		klog.Errorln("nvml Init err: ", nvret)
@@ -212,15 +212,18 @@ func GetMigUUIDFromIndex(uuid string, idx int) string {
 			klog.Fatalf("nvidia-smi -L failed with %s\n", err)
 		}
 		outStr := stdout.String()
-		uuid := GetMigUUIDFromSmiOutput(outStr, originuuid, idx)
-		return uuid
+		migUUID := GetMigUUIDFromSmiOutput(outStr, originuuid, idx)
+		if migUUID == "" {
+			return "", fmt.Errorf("failed to resolve MIG UUID for device %s at index %d via nvidia-smi fallback", originuuid, idx)
+		}
+		return migUUID, nil
 	}
 	res, ret := migdev.GetUUID()
 	if ret != nvml.SUCCESS {
 		klog.Error(`nvml get mig uuid error ret=`, ret)
 		panic(0)
 	}
-	return res
+	return res, nil
 }
 
 func GetMigGpuInstanceIdFromIndex(uuid string, idx int) (int, error) {
@@ -467,7 +470,7 @@ func deepCopyMigConfig(src nvidia.MigConfigSpec) nvidia.MigConfigSpec {
 	return dst
 }
 
-func (nv *NvidiaDevicePlugin) GetContainerDeviceStrArray(c device.ContainerDevices) []string {
+func (nv *NvidiaDevicePlugin) GetContainerDeviceStrArray(c device.ContainerDevices) ([]string, error) {
 	tmp := []string{}
 	needsreset := false
 	position := 0
@@ -510,16 +513,15 @@ func (nv *NvidiaDevicePlugin) GetContainerDeviceStrArray(c device.ContainerDevic
 					}
 				}
 			}
-			migUUID := GetMigUUIDFromIndex(val.UUID, position)
-			if migUUID == "" {
-				klog.Errorf("failed to resolve MIG UUID for %s at position %d, skipping", val.UUID, position)
-				continue
+			migUUID, err := GetMigUUIDFromIndex(val.UUID, position)
+			if err != nil {
+				return nil, fmt.Errorf("failed to resolve MIG UUID for %s at position %d: %w", val.UUID, position, err)
 			}
 			tmp = append(tmp, migUUID)
 		}
 	}
 	klog.V(3).Infoln("mig current=", nv.migCurrent, ":", needsreset, "position=", position, "uuid lists", tmp)
-	return tmp
+	return tmp, nil
 }
 
 var podAllocationTrySuccess = func(nodeName string, devName string, lockName string, pod *corev1.Pod) {
