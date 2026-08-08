4ff43ab006e21f1eef585ffd3d777dec7bb3c848
aniket866 <iamaniketkumarmaner@gmail.com>
2026-08-08 18:18:35 +0530

    fix/node-lock-release
    Signed-off-by: aniket866 <iamaniketkumarmaner@gmail.com>

--- pkg/device/ascend/device_test.go
+++ pkg/device/ascend/device_test.go
@@ -17,6 +17,7 @@ limitations under the License.
 package ascend
 
 import (
+	"context"
 	"errors"
 	"fmt"
 	"strconv"
--- pkg/device/iluvatar/device_test.go
+++ pkg/device/iluvatar/device_test.go
@@ -17,6 +17,7 @@ limitations under the License.
 package iluvatar
 
 import (
+	"context"
 	"errors"
 	"flag"
 	"fmt"
