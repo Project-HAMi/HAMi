27946a106152256ec5165c704f35f20d9cae3c23
Ricky Dama <rickydama2006@gmail.com>
2026-08-11 05:06:45 +0530

    fix(monitor): resolve metrics corruption on non-40 char UUIDs Fixes #2561 by replacing hardcoded length checks with IsValidUUID and dynamically trimming trailing null bytes from the device UUID instead of unconditionally truncating to 40 characters.
    Signed-off-by: Ricky Dama <rickydama2006@gmail.com>

--- cmd/vGPUmonitor/metrics.go
+++ cmd/vGPUmonitor/metrics.go
@@ -425,12 +425,11 @@ func (cc ClusterManagerCollector) collectContainerMetrics(ch chan<- prometheus.M
 
 	// Iterate through each device
 	for i := range c.Info.DeviceNum() {
-		uuid := c.Info.DeviceUUID(i)
-		if len(uuid) < 40 {
-			klog.Warningf("Device %d in Pod %s/%s, Container %s has invalid UUID length %d (shared memory not yet initialised); skipping until next scrape", i, pod.Namespace, pod.Name, ctr.Name, len(uuid))
+		if !c.Info.IsValidUUID(i) {
+			klog.Warningf("Device %d in Pod %s/%s, Container %s has invalid UUID (shared memory not yet initialised); skipping until next scrape", i, pod.Namespace, pod.Name, ctr.Name)
 			continue
 		}
-		uuid = uuid[0:40] // Ensure UUID is truncated to 40 characters
+		uuid := strings.TrimRight(c.Info.DeviceUUID(i), "\x00")
 		if !utf8.ValidString(uuid) {
 			klog.Warningf("Device %d in Pod %s/%s, Container %s has invalid UTF-8 UUID (shared memory not yet initialised); skipping until next scrape", i, pod.Namespace, pod.Name, ctr.Name)
 			continue
