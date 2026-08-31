bc0adc68cde465f403d6648c1520df22953afd73
KunwarSidhu47 <kunwarsidhu1947@gmail.com>
2026-08-31 23:58:41 +0530

    Move nil-requests comment inside ResourceMemoryName guard
    Signed-off-by: KunwarSidhu47 <kunwarsidhu1947@gmail.com>

--- pkg/device/ascend/device.go
+++ pkg/device/ascend/device.go
@@ -179,9 +179,9 @@ func (dev *Devices) MutateAdmission(ctr *corev1.Container, p *corev1.Pod) (bool,
 			return true, errors.New("vNPU not supported for multiple devices")
 		}
 	}
-	// Requests may be nil when the pod declares only limits; writing to a
-	// nil map panics.
 	if dev.config.ResourceMemoryName != "" {
+		// Requests may be nil when the pod declares only limits; writing to a
+		// nil map panics.
 		if ctr.Resources.Requests == nil {
 			ctr.Resources.Requests = corev1.ResourceList{}
 		}
